abd93baedaf4b8abe515a80df45b3a3fb692b43a
braincooler <w,zeller@online.de>
2020-08-03 15:17:00 +0200

    set test property

---
+++ src/main/java/de/braincoooler/sandbox/config/Config.java
@@ -0,0 +1,26 @@
+package de.braincoooler.sandbox.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+public class Config {
+    private static final Logger log = LoggerFactory.getLogger(Config.class);
+
+    @Value("§{gw.user}")
+    private final String gwUser;
+
+    public Config(String gwUser) {
+        this.gwUser = gwUser;
+    }
+
+    @PostConstruct
+    public void init(){
+        log.info("<< ----- gw.user:{} ----- >>", gwUser);
+    }
+
+}
